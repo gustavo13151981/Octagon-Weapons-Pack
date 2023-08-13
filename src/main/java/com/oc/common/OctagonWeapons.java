7c6afb2bdbce3d20ace54a22010190a1dd890c11
Giant_Salted_Fish <wangzhichuan6@outlook.com>
2023-08-14 02:46:31 +0800

    Project setup.

---
+++ src/main/java/com/oc/common/OctagonWeapons.java
@@ -0,0 +1,52 @@
+package com.oc.common;
+
+import com.fmum.common.FMUM;
+import com.fmum.common.pack.FolderPack;
+import com.fmum.common.pack.IContentPack;
+import com.fmum.common.pack.ILoadablePack;
+import com.fmum.common.pack.JarPack;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.ModContainer;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.io.File;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+@Mod(
+	modid = "oc",
+	name = "Octagon Weapons Pack",
+	version = "0.3.3",
+	acceptedMinecraftVersions = "1.12.2",
+	dependencies = "required-after:" + FMUM.MODID
+)
+public final class OctagonWeapons implements ILoadablePack
+{
+	private final ILoadablePack wrapped_pack;
+	
+	public OctagonWeapons()
+	{
+		// Wrapped here to make it work in dev environment. If your pack only runs as released mod \
+		// Then you can directly inherit JarPack and delete #wrapped_pack.
+		final ModContainer mod_container = Loader.instance().activeModContainer();
+		this.wrapped_pack = (
+			mod_container.getSource().isFile()
+			? new JarPack( mod_container )
+			: new FolderPack( mod_container )
+		);
+	}
+	
+	@Override
+	public Function< ILoadContext, Supplier< IContentPack > >
+		prepareLoadServerSide( IPrepareContext ctx )
+	{ return this.wrapped_pack.prepareLoadServerSide( ctx ); }
+	
+	@Override
+	@SideOnly( Side.CLIENT )
+	public Function< ILoadContext, Supplier< IContentPack > >
+		prepareLoadClientSide( IPrepareContext ctx )
+	{ return this.wrapped_pack.prepareLoadClientSide( ctx ); }
+}
