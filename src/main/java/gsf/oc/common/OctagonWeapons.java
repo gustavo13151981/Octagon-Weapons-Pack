5621b59aec243f0cb4ac291e5e0281d51a410a55
Giant_Salted_Fish <wangzhichuan6@outlook.com>
2024-09-04 08:18:09 +0800

    Renew branch adapt.

--- src/main/java/gsf/oc/common/OctagonWeapons.java
+++ src/main/java/gsf/oc/common/OctagonWeapons.java
@@ -1,16 +1,16 @@
 package gsf.oc.common;
 
 import com.fmum.FMUM;
-import com.fmum.load.ModPackFactory;
+import com.fmum.load.IPackFactory;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(
 	modid = "oc",
 	name = "Octagon Weapons Pack",
-	version = "0.3.3",
+	version = "0.4.0-dev",
 	acceptedMinecraftVersions = "1.12.2",
 	dependencies = "required-after:" + FMUM.MODID + ";"
 )
-public final class OctagonWeapons extends ModPackFactory {
+public final class OctagonWeapons implements IPackFactory {
 	// Pass.
 }
